a4289ed6fb598ad92a73b450bdd8d2d12e34bddd
Kyle Isom <kyle@cloudflare.com>
2017-05-03 16:46:43 -0700

    Add "ensure" command.
    This ensures all certificate specs have valid key pairs, exiting
    with a failure if it can't.

---
+++ cli/ensure.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var ensureTolerance = 3
+
+var ensureCmd = &cobra.Command{
+	Use:   "ensure",
+	Short: "Ensure all certificate specs have matching keypairs.",
+	Long: `Certificate manager will load all certificate specs, and ensure that the
+TLS key pairs they identify exist, are valid, and that they are up-to-date.`,
+	Run: Ensure,
+}
+
+func Ensure(cmd *cobra.Command, args []string) {
+	mgr, err := newManager()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed: %s\n", err)
+		os.Exit(1)
+	}
+
+	err = mgr.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed: %s\n", err)
+		os.Exit(1)
+	}
+
+	err = mgr.MustCheckCerts(ensureTolerance)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("OK")
+}
+
+func init() {
+	RootCmd.AddCommand(ensureCmd)
+	ensureCmd.Flags().IntVarP(&ensureTolerance, "tries", "n", ensureTolerance, "number of times to retry refreshing a certificate")
+}
