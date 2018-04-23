1b73d73d2fabe5a698a0bd2f741e5f559ee452a2
Andrew Plunk <aplunk@cloudflare.com>
2018-04-23 09:44:08 -0500

    Fix metrics wrapper data race

--- metrics/types.go
+++ metrics/types.go
@@ -1,21 +1,30 @@
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 // This file defines utility metrics structures that allow a snapshot
 // of values to be displayed on the index page.
 
 type counter struct {
-	v int
-	c prometheus.Counter
+	v  int
+	c  prometheus.Counter
+	mx sync.Mutex
 }
 
 func (c *counter) Inc() {
+	c.mx.Lock()
+	defer c.mx.Unlock()
 	c.v++
 	c.c.Inc()
 }
 
 func (c *counter) Get() int {
+	c.mx.Lock()
+	defer c.mx.Unlock()
 	return c.v
 }
 
@@ -38,25 +47,34 @@ func newCounter(name, help string) *counter {
 }
 
 type gauge struct {
-	v float64
-	g prometheus.Gauge
+	v  float64
+	g  prometheus.Gauge
+	mx sync.Mutex
 }
 
 func (g *gauge) Inc() {
+	g.mx.Lock()
+	defer g.mx.Unlock()
 	g.v++
 	g.g.Inc()
 }
 
 func (g *gauge) Dec() {
+	g.mx.Lock()
+	defer g.mx.Unlock()
 	g.v--
 	g.g.Dec()
 }
 
 func (g *gauge) Get() float64 {
+	g.mx.Lock()
+	defer g.mx.Unlock()
 	return g.v
 }
 
 func (g *gauge) Set(v float64) {
+	g.mx.Lock()
+	defer g.mx.Unlock()
 	g.v = v
 	g.g.Set(v)
 }
